4667a3f50f3129dd315abda9daa88cad3f184e7b
jerry george <jerrygithub360@gmail.com>
2025-04-07 15:05:15 +0100

    update

--- chromaclass.py
+++ chromaclass.py
@@ -1,48 +1,98 @@
-from abc import abstractmethod,ABC
+from abc import ABC, abstractmethod
 from sentence_transformers import SentenceTransformer
 import chromadb
-client = {
-    'chroma':{
-    'client': lambda :chromadb.PersistentClient(),
-    'embedding_function':lambda : SentenceTransformer('paraphrase-albert-small-v2')
+
+model = SentenceTransformer('paraphrase-albert-small-v2')
+
+client_config = {
+    'chroma': {
+        'client': lambda: chromadb.PersistentClient(),
+        'embedding_function': lambda: model.encode
     }
 }
 
 class ChromaObj(ABC):
     @abstractmethod
-    def create_collection(self,name:str,metadata:dict):
-        ...
+    def create_collection(self, name: str, metadata: dict): ...
     @abstractmethod
-    def add_to_collection(self,name:str):
-        ...
+    def add_to_collection(self, param: dict, name: str = None): ...
     @abstractmethod
-    def update_to_collection(self):
-        ...
+    def update_to_collection(self, param: dict, name: str = None): ...
     @abstractmethod
-    def get_collection(self):
-        ...
+    def get_collection(self, name: str): ...
     @abstractmethod
-    def delete_collection(self):
-        ...
+    def delete_from_collection(self, param: dict, name: str = None): ...
     @abstractmethod
-    def query_collection(self):
-        ...
+    def query_collection(self, param: dict, name: str = None): ...
+    @abstractmethod
+    def use_collection(self, name: str): ...
 
 class Chroma(ChromaObj):
-    def __init__(self,client):
-        self.client = client['chroma']['client']
-    def create_collection(self,name:str,metadata:dict):
-        self.client.create_collection(
-            name= name,
-            embedding_function = client['chroma']['embedding_function'],
-            metadata = metadata
+    def __init__(self, client):
+        self.client = client['chroma']['client']()
+        self.embedding_function = client['chroma']['embedding_function']()
+        self.collections = {}
+        self.current_collection = None
+
+    def create_collection(self, name: str, metadata: dict):
+        collection = self.client.create_collection(
+            name=name,
+            embedding_function=self.embedding_function,
+            metadata=metadata
         )
-    def add_to_collection(self,name:str):
-        ...
-    def update_to_collection(self):
-        ...
-    def get_collection(self):
-        ...
-    def delete_collection(self):
-        ...
-    def query_collection
+        self.collections[name] = collection
+        self.current_collection = name
+
+    def use_collection(self, name: str):
+        if name in self.collections:
+            self.current_collection = name
+        else:
+            collection = self.client.get_collection(
+                name=name,
+                embedding_function=self.embedding_function
+            )
+            self.collections[name] = collection
+            self.current_collection = name
+
+    def _get_collection(self, name: str = None):
+        name = name or self.current_collection
+        if not name:
+            raise ValueError("No collection specified or in use.")
+        if name not in self.collections:
+            raise ValueError(f"Collection '{name}' not found.")
+        return self.collections[name]
+
+    def add_to_collection(self, param: dict, name: str = None):
+        collection = self._get_collection(name)
+        collection.add(
+            documents=param.get('documents'),
+            embeddings=param.get('embeddings'),
+            metadatas=param.get('metadatas'),
+            ids=param.get('ids')
+        )
+
+    def update_to_collection(self, param: dict, name: str = None):
+        collection = self._get_collection(name)
+        collection.update(
+            documents=param.get('documents'),
+            embeddings=param.get('embeddings'),
+            metadatas=param.get('metadatas'),
+            ids=param.get('ids')
+        )
+
+    def get_collection(self, name: str):
+        return self._get_collection(name)
+
+    def delete_from_collection(self, param: dict, name: str = None):
+        collection = self._get_collection(name)
+        return collection.delete(
+            ids=param.get('ids'),
+            where=param.get('where')
+        )
+
+    def query_collection(self, param: dict, name: str = None):
+        collection = self._get_collection(name)
+        return collection.query(
+            query_texts=param.get('query'),
+            n_results=param.get('n_results', 5)
+        )
